558056164293f9f85520cd94c24324924d555c23
JamesPerenchio123 <S3932963@student.rmit.edu.au>
2024-05-02 00:03:49 +1000

    e

--- js/script.js
+++ js/script.js
@@ -52,15 +52,22 @@ function processCommand(command) {
             case "help": // Display all commands with "help" (case-insensitive)
             case "h": // Add an alias for "help"
                 console.log("User entered:", command); // Log user command to console
-                result = "<b>Available commands:</b><br>";
+                result = "<b>\n\nAvailable commands:</b><br>";
                 result += "<b>help (h):</b> Display available commands<br>";
                 result += "<b>clear:</b> Clear the terminal<br>";
                 result += "<b>greet:</b> Display a welcome message<br>";
                 result += "<b>about:</b> Redirect to the about page<br>";
+                result += "<b>summary -t:</b> Display the about page in the terminal<br>"
+                result += "<b>projects (p):</b> List projects<br>";
                 break;
             case "greet":
                 console.log("User entered:", command); // Log user command to console
-                result = "Hello! Welcome to the interactive terminal!<br>";
+                result = "\n\nHello! Welcome to the interactive terminal!<br>";
+                break;
+            
+            case "summary -t":
+                console.log("User entered:", command); // Log user command to console
+                result = "\n\nHi, I'm James! \nCurrently in my third year of a Bachelor's in IT at RMIT, I'm passionate about building dynamic websites that are not only user-friendly but also visually appealing. My interest in IT extends to networking, where I recently earned my CompTIA Network+ certification, solidifying my skills in network troubleshooting and configuration.  I'm actively working towards my CCNP to further my expertise. I thrive in collaborative environments and have a proven track record of delivering successful results.  For instance, in a recent hackathon, my team of 5 tackled the challenge of improving lecture comprehension and learning. Our solution leveraged RagAI to create a system that answered user questions about lectures and automatically generated flashcards for better retention. This project not only required strong problem-solving but also collaboration, as we combined our skills to win the 'Best Use of RedactiveAI' award. My problem-solving skills are constantly evolving as I participate in HackTheBox challenges and seek out hackathons whenever possible \n<br>";
                 break;
             case "about":
                 console.log("User entered:", command); // Log user command to console
@@ -82,7 +89,7 @@ function processCommand(command) {
             case "projects": // list projects page (case-insensitive)
             case "p": // Add an alias for "projects"
                 console.log("User entered:", command); // Log user command to console
-                result = "<b>List of Projects:</b><br>";
+                result = "<b>\n\nList of Projects:</b><br>";
                 result += "1. Project 1<br>";
                 result += "2. Project 2<br>";
                 result += "3. Project 3<br>";
