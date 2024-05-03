4d5afa65f2998227ddf6cf2f9895add468a61ff7
JamesPerenchio123 <S3932963@student.rmit.edu.au>
2024-05-04 02:21:56 +1000

    x

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 console.log("log test: working"); 
+
 const output = document.getElementById('output');
 const userInput = document.getElementById('userInput');
 const prompt = document.getElementById('prompt');
@@ -9,20 +10,24 @@ let hostname = "mymachine"; // Replace with your hostname
 
 // Update prompt dynamically
 function updatePrompt() {
-    prompt.textContent = `${username}@${hostname}:~$ (help/h)`; // Include commands in prompt
-  }
-  
+    prompt.innerHTML = `${username}@${hostname}:~$ <span id="cursor"></span>`;
+}
 
 updatePrompt(); // Set initial prompt
 
 // Focus on user input on page load
 userInput.focus(); 
 
-
-
-
 userInput.addEventListener('keydown', (event) => {
+    // Ignore special keys
+    if (event.key === 'Meta' || event.key === 'Tab' || event.key === 'Alt') {
+        return;
+    }
+
     if (event.key === 'Enter') {
+        // Prevent default action of Enter key
+        event.preventDefault();
+        
         // Add user input to output
         output.innerHTML += `${prompt.textContent}${currentLine}<br>`;
         // Process the command
@@ -40,7 +45,6 @@ userInput.addEventListener('keydown', (event) => {
 });
 
 
-
 function processCommand(command) {
     let result = ""; // Variable to store the result of processing the command
 
@@ -52,23 +56,61 @@ function processCommand(command) {
             case "help": // Display all commands with "help" (case-insensitive)
             case "h": // Add an alias for "help"
                 console.log("User entered:", command); // Log user command to console
-                result = "<b>\n\nAvailable commands:</b><br>";
-                result += "<b>help (h):</b> Display available commands<br>";
-                result += "<b>clear:</b> Clear the terminal<br>";
-                result += "<b>greet:</b> Display a welcome message<br>";
-                result += "<b>about:</b> Redirect to the about page<br>";
-                result += "<b>summary -t:</b> Display the about page in the terminal<br>"
-                result += "<b>projects (p):</b> List projects<br>";
+                result += "<b>\n\nAvailable commands \n</b><br>";
+
+                result +="<b>Terminal commands:</b><br>";
+                result += "<b>help (h):</b>   Display available commands<br>";
+                result += "<b>clear:</b>      Clear the terminal<br>";
+                result += "<b>greet:</b>      Display a welcome message<br>";
+                result += "<b>cowsay:</b>     Make a cow say something<br>\n";
+                result += "<b>navigation: </b> <br>";
+                result += "<b>experience(exp):</b>   List Experience in the terminal<br>";
+                result += "<b>education(edu):</b>    List Education and Certifications in the terminal<br>";
+                result += "<b>projects (p):</b>      List projects in the terminal<br>";
+                result += "<b>summary (s):</b>       Display the about page in the terminal<br>"
+                result += "<b>about:</b>             Redirect to the about page<br>";
                 break;
             case "greet":
                 console.log("User entered:", command); // Log user command to console
                 result = "\n\nHello! Welcome to the interactive terminal!<br>";
                 break;
             
-            case "summary -t":
+            case "cowsay":
+                console.log("User entered:", command); // Log user command to console
+                result = "<pre>\n";
+                result += " _____________\n";
+                result += "< I'm a cow! >\n";
+                result += " -------------\n";
+                result += "        \\   ^__^\n";
+                result += "         \\  (oo)\\_______\n";
+                result += "            (__)\\       )\\/\\\n";
+                result += "                ||----w |\n";
+                result += "                ||     ||\n";
+                result += "</pre>";
+                break;
+            case "experience": // Display experience
+            case "exp": // Add an alias for "experience"
+                console.log("User entered:", command); // Log user command to console
+                result += "\n\nWork Experience<br>";
+                result += "\n1. IT Support Officer ---------------------  Monash University        (April 2024 - current)<br>";
+                result += "2. Customer Service Representative --------- Rooftop at QT Hotels     (Sep 2022 - Dec 2023)<br>";
+                result += "3. Bartender ------------------------------- Miss Kuku, HooHaa        (March 2021 - Sep 2022)<br>";
+                break;
+
+            case "education": // Display education and certifications
+            case "edu": // Add an alias for "education"
+                console.log("User entered:", command); // Log user command to console
+                result = "\n\nEducation and Certifications<br>";
+                result += "\n1. Bachelor of IT -------------------------------- RMIT University     (2022 - 2024)<br>";
+                result += "2. CompTIA Network+ Certification ---------------- CompTIA             (Feb 2024)\n<br>";
+                break;
+
+            case "summary (s)": // Display summary
+            case "s": // Add an alias for "summary
                 console.log("User entered:", command); // Log user command to console
                 result = "\n\nHi, I'm James! \nCurrently in my third year of a Bachelor's in IT at RMIT, I'm passionate about building dynamic websites that are not only user-friendly but also visually appealing. My interest in IT extends to networking, where I recently earned my CompTIA Network+ certification, solidifying my skills in network troubleshooting and configuration.  I'm actively working towards my CCNP to further my expertise. I thrive in collaborative environments and have a proven track record of delivering successful results.  For instance, in a recent hackathon, my team of 5 tackled the challenge of improving lecture comprehension and learning. Our solution leveraged RagAI to create a system that answered user questions about lectures and automatically generated flashcards for better retention. This project not only required strong problem-solving but also collaboration, as we combined our skills to win the 'Best Use of RedactiveAI' award. My problem-solving skills are constantly evolving as I participate in HackTheBox challenges and seek out hackathons whenever possible \n<br>";
                 break;
+
             case "about":
                 console.log("User entered:", command); // Log user command to console
                 result = "Redirecting to the about page in 3 seconds...<br>";
@@ -87,13 +129,16 @@ function processCommand(command) {
                     }, 1000);
                 }, 1000);
                 return; // Return to prevent further processing
+
+
             case "projects": // list projects page (case-insensitive)
             case "p": // Add an alias for "projects"
                 console.log("User entered:", command); // Log user command to console
                 result = "<b>\n\nList of Projects:</b><br>";
-                result += "1. Project 1<br>";
-                result += "2. Project 2<br>";
-                result += "3. Project 3<br>";
+                result += "1. Took part in UNIHack hackathon and won best use of RedactiveAI, cash prize of $1000<br>";
+                result += "2. Utilized python and machine learning to optimize battery storage and production for AMBER Energy<br>";
+                result += "3. Created a dynamic tourism website using a SQL database that updates based on the users inputs.<br>";
+                result += "4. Initiated and successfully completed a mock hotel reservation program using java, showcasing high proficiency.<br>";
                 break;
             case "clear":
                 console.clear(); // Clear console
@@ -105,9 +150,7 @@ function processCommand(command) {
     }
 
     // Append result to output
-    output.innerHTML += `${prompt.textContent}${result}`;
-    // Update prompt after processing
-    updatePrompt();
+    updateTerminal(result);
 }
 
 function updateTerminal(result) {
@@ -116,8 +159,7 @@ function updateTerminal(result) {
     updatePrompt(); // Update prompt after processing
 }
 
-
   
   
 // Pre-populate some output (optional)
-output.innerHTML += "Welcome to the interactive terminal! Feel free to use this. start by pressig h.<br>";
+output.innerHTML += "Welcome to the interactive terminal! To access the help menu, type 'help' or 'h'.<br>";
